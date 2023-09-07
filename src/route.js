8f51e66cb19e8e5358cced62dba9d914aa686af5
*Adenipekun <*blessingadenipekun@gmail.com*>
2023-09-08 00:01:21 +0100

    update

--- src/route.js
+++ src/route.js
@@ -1,6 +1,8 @@
 const route = require("express").Router()
-const currentUtcTime = new Date();
-const currentDay = currentUtcTime.toLocaleDateString('en-US', {weekday: "long"});
+const currentDate = new Date();
+const currentTime = currentDate.toISOString()
+const currentUtcDate = currentTime.slice(0, -5) + "Z"
+const currentDay = currentDate .toLocaleDateString('en-US', {weekday: "long"});
 const githubFileUrl = "https://github.com/nipekbless/hngx_internship_task1/blob/main/index.js";
 const githubRepoUrl = "https://github.com/nipekbless/hngx_internship_task1";
 const statusCode = 200
@@ -16,14 +18,14 @@ route.get("/api", (req, res)=>{
         {
             "slack_name" : slack_name,
             "current_day": currentDay,
-            "utc_time": currentUtcTime,
+            "utc_time": currentUtcDate,
             "track": track,
             "github_file_url": githubFileUrl,
             "github_repo_url": githubRepoUrl,
             "status_code": statusCode
         }
     )
-    console.log(currentDay)
+    console.log(currentUtcDate)
 })
 
 module.exports = route
